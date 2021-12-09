c7374a2ba317479bf4ea14f82e62c18bc4390fcd
Nitishkumar62954 <95841362+Nitishkumar62954@users.noreply.github.com>
2021-12-08 23:25:20 -0800

    Add files via upload
    Q1. Implement Round robin scheduling algorithm with quanta 3. Compute waiting time, turnaround time. 
              Round Robin is a CPU scheduling algorithm where each process is assigned a fixed time slot in a cyclic way  
    In this program , i have simulated the round robin algorithm with value of quantum 3.
    Completion Time: Time at which process completes its execution.
    Turn Around Time: Time Difference between completion time and arrival time. Turn Around Time = Completion Time – Arrival Time
    Waiting Time(W.T): Time Difference between turn around time and burst time. 
    Waiting Time = Turn Around Time – Burst Time
    
    
    Q2. Write a program to demonstrate fork where parent and child run same codes and parent process should be executed first.
           Fork system call is used for creating a new process, which is called child process, which runs concurrently with the process that makes the fork() call (parent process). After a new child process is created, both processes will execute the next instruction following the fork() system call. 
    In this, a child process is created. fork() returns 0 in the child process and positive integer in the parent process.

---
+++ OS_PRACTICAL(EXAM)_2.cpp
@@ -0,0 +1,27 @@
+/*      SET-3   (PROGRAM-2)   */
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main()
+{
+    pid_t pid;
+	/* fork a child process */
+	pid = fork();
+    if (pid < 0)
+    {
+		/* error occurred */
+		fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0)
+    {
+		/* child process */
+		execlp("/bin/ls","ls",NULL);
+    }
+    else
+    {
+        printf("Parent Process.");
+    }
+    return 0;
+}
